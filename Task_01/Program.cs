93630a69d4b12634023f1ca4ebf69ce99c34a309
Alexey Zobnin <ayuzobnin@edu.hse.ru>
2020-11-05 17:00:23 +0300

    Solved 1.3.1

--- Task_01/Program.cs
+++ Task_01/Program.cs
@@ -34,10 +34,22 @@ namespace Task_01
         {
             int l;
             // TODO : Считать l;
-            l = -0;
+            l = int.Parse(Console.ReadLine());
             int result = CalcS(l);
 
             // TODO : вывести результат или 0, если его не было получено.
+            Console.WriteLine(result == -1 ? 0 : result);
+        }
+
+        static bool AreAllDigitsSimilar(int n)
+        {
+            bool ok = true;
+            string val = n.ToString();
+            for (int i = 1; i < val.Length; ++i)
+            {
+                ok &= val[i] == val[0];
+            }
+            return ok;
         }
 
         public static int CalcS(int l)
@@ -46,7 +58,20 @@ namespace Task_01
 
             // TODO : Написать логику для метода вычисления S и 
             // если вычислить удалось, поместить в res полученное S.
-
+            int step = 0;
+            int sum = 0;
+            while (sum < 10 * 1000)
+            {
+                sum += l + step++;
+                if (sum >= 1000)
+                {
+                    if (AreAllDigitsSimilar(sum))
+                    {
+                        res = sum;
+                        break;
+                    }
+                }
+            }
             return res;
         }
 
